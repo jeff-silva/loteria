bdf8423f69e1bc2177b1ed19f4b1a9e87ef083a4
Jeferson Inácio <jeferson.i.silva@gmail.com>
2021-12-29 13:05:19 -0300

    Heroku deploy

---
+++ app/Console/Commands/AppDeploy.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Console\Commands;
+
+class AppDeploy extends AppBase
+{
+    protected $signature = 'app:deploy';
+    protected $description = 'Utilize este comando no servidor para deploy da aplicação';
+
+    public function handle()
+    {   
+        $this->call('optimize');
+        $this->call('app:db-import');
+        $this->call('migrate');
+        $this->call('db:seed');
+        $this->call('optimize');
+    }
+}
